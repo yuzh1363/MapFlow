3d5238955739e1d7cf0600de2830211a825b2961
yuzh1363 <yuzh0806@gmail.com>
2026-03-31 10:43:56 +0800

    新增：樹洞輸入框元件

--- final-project/src/App.vue
+++ final-project/src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
+
 </script>
 
 <template>
-  <Header></Header>
+  
   
   
 </template>
---
+++ final-project/src/components/InputCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="input-card">
+        <form>
+            <div class="mb-3">
+                <input class="form-control" type="text" placeholder="輸入你的職業" aria-label="default input example">
+            </div>
+            <div class="mb-3">
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="說說你遇到的問題"></textarea>
+            </div>
+            <MyButton text="送出" type="green" border="square" size="size-sm"></MyButton>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import MyButton from './MyButton.vue'
+</script>
+
+<style scoped>
+.input-card{
+    width: 500px;
+    padding: 32px 24px;
+    background-color: #fff;
+    border-radius: 12px;
+    box-shadow: 1px 1px 12px #dfdfdf ;
+}
+</style>
