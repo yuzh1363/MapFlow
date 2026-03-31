353422232c671c60cf045d6907201703839e8d1e
yuzh1363 <yuzh0806@gmail.com>
2026-03-31 13:34:09 +0800

    新增：里程碑卡片，修正：元件使用方法

---
+++ final-project/src/components/MilestoneCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="milestone-card ">
+        <img src="../assets/images/user.svg" alt="">
+        <p class="lable-lg">完成角色</p>
+        <p class="lable-num">10</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.milestone-card{
+    background-color: var(--color-neutral-500);
+    width: 180px;
+    border-radius: 16px;
+    padding: 8px 20px;
+}
+</style>
--- final-project/vite.config.js
+++ final-project/vite.config.js
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 這行會自動把你的 SCSS 注入到每個 Vue 元件中
+        // additionalData: `@use "./src/assets/scss/style.scss" as *;`
+      }
+    }
+  }
 })
