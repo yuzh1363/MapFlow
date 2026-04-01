400d1df1fda6f8a2572f7072ebf38a12a553482b
yuzh1363 <yuzh0806@gmail.com>
2026-04-01 11:23:07 +0800

    新增：樣板市集頁面

--- final-project/src/App.vue
+++ final-project/src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
   import Footer from './components/Footer.vue';
 import Header from './components/Header.vue'
-import TreeHole from './views/TreeHole.vue';
 </script>
 
 <template>
   <Header class="sticky-top"></Header>
   <!-- <RouterView> -->
-  
+
   <Footer class="fixed-bottom"></Footer>
 </template>
