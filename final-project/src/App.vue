87dad84166d792b00e2959300d99ba4cd0965983
yuzh1363 <yuzh0806@gmail.com>
2026-04-01 11:39:21 +0800

    新增：搜尋列元件

--- final-project/src/App.vue
+++ final-project/src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
   import Footer from './components/Footer.vue';
 import Header from './components/Header.vue'
+import SearchBar from './components/SearchBar.vue';
 </script>
 
 <template>
-  <Header class="sticky-top"></Header>
+  <!-- <Header class="sticky-top"></Header> -->
   <!-- <RouterView> -->
-
+  <SearchBar></SearchBar>
   <Footer class="fixed-bottom"></Footer>
 </template>
---
+++ final-project/src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+        <button></button>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
