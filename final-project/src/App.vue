1da5fc0cbd34b5730a400c11616b8cfa9ee60be9
YUTING20010506 <williamchang90@gmail.com>
2026-04-07 11:20:13 +0800

    新增：資料修改、更改密碼頁面

--- final-project/src/App.vue
+++ final-project/src/App.vue
@@ -1,22 +1,20 @@
 <script setup>
 import Footer from './components/Footer.vue';
 import Header from './components/Header.vue'
-import Thoughts from './components/Thoughts.vue';
-import Role from './views/Role.vue';
-import Skill from './views/Skill.vue';
+import EditData from './views/EditData.vue';
+import EditCode from './views/EditCode.vue';
 
 
 </script>
 
 <template>
-  <!-- <Header></Header> -->
-  <!-- <main>
-    <RouterView></RouterView>
-  </main> -->
-  <!-- <Skill></Skill> -->
-  <!-- <Role></Role> -->
-   <Thoughts></Thoughts>
-  <!-- <Footer></Footer> -->
+  <Header></Header>
+  <main>
+    <!-- <RouterView></RouterView> -->
+    <EditCode/>
+  </main>
+  
+  <Footer></Footer>
 </template>
 
 <style scoped></style>
