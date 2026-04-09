ff295419023cc167f6a30dc532088eb2a93572c9
yuzh1363 <yuzh0806@gmail.com>
2026-04-09 10:34:11 +0800

    新增：網站部署設定

--- final-project/src/routers/main_router.js
+++ final-project/src/routers/main_router.js
@@ -121,7 +121,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory('/MapFlow/'),
     routes,
     scrollBehavior(to, from, savedPosition) {
         if (savedPosition) {
--- final-project/vite.config.js
+++ final-project/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/MapFlow/',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
